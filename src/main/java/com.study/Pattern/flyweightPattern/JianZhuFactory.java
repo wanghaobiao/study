431e2f6d8a07797ee04429415fa5d4ac7bad178d
549819221 <549819221@qq.com>
2018-12-23 16:32:38 +0800

    wanghb

---
+++ src/main/java/com.study/Pattern/flyweightPattern/JianZhuFactory.java
@@ -0,0 +1,17 @@
+package com.study.pattern.flyweightPattern;
+import java.util.*;
+//建筑工厂
+public class JianZhuFactory {
+    private static final Map<String,TiYuGuan> tygs = new HashMap<String,TiYuGuan>();
+    public static TiYuGuan getTyg(String yundong){
+        TiYuGuan tyg = tygs.get(yundong);
+        if(tyg == null){
+            tyg = new TiYuGuan(yundong);
+            tygs.put(yundong,tyg);
+        }
+        return tyg;
+    }
+    public static int getSize(){
+        return tygs.size();
+    }
+}
