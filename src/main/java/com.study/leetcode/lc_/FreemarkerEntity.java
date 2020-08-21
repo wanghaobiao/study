06704c4618ea22c9d45da82da857c5b2f40d3692
wanghb <549819221@qq.com>
2020-08-21 15:49:47 +0800

    代码提交 wanghb

---
+++ src/main/java/com.study/leetcode/lc_/FreemarkerEntity.java
@@ -0,0 +1,17 @@
+package com.study.leetcode.lc_;
+
+public class FreemarkerEntity {
+    private Integer index;
+
+    public FreemarkerEntity (Integer index){
+        this.index = index;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+}
